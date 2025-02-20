e7c03e0c9d956f1c2e67e2862a1ddba5ee324342
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2025-02-20 14:39:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-//your JS code here. If required.
+        const codes = document.querySelectorAll(".code");
 
-const codes = document.querySelectorAll(".code");
-
-codes.forEach((input,index)=>{
-	input.addEventListener("input",(e)=>
-		{
-			
-			if(e.target.value.length===1 && index < codes.length-1)
-			{
-				codes[index+1].focus();
-			}
-		})
-
-
-	input.addEventListener("keydown",(e)=>{
-		if(e.key == "Backspace" && index > 0 && e.target.value === "")
-		{
-			codes[index-1].focus();
-		}
-	})
-});
+        codes.forEach((input, index) => {
+            input.addEventListener("input", (e) => {
+                if (e.target.value.length === 1 && index < codes.length - 1) {
+                    codes[index + 1].focus();
+                }
+            });
+            
+            input.addEventListener("keydown", (e) => {
+                if (e.key === "Backspace" && index > 0 && e.target.value === "") {
+                    codes[index - 1].focus();
+                }
+            });
+        });
