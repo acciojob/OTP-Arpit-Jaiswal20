cc7a16c1d8de4e5e339f0a38c839cf835a3b90c5
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2025-02-21 22:36:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-        // const codes = document.querySelectorAll(".code");
+        const codes = document.querySelectorAll(".code");
 
-        // codes.forEach((input, index) => {
-        //     input.addEventListener("input", (e) => {
-        //         if (e.target.value.length === 1 && index < codes.length - 1) {
-        //             codes[index + 1].focus();
-        //         }
-        //     });
+        codes.forEach((input, index) => {
+            input.addEventListener("input", (e) => {
+                if (e.target.value.length === 1 && index < codes.length - 1) {
+                    codes[index + 1].focus();
+                }
+            });
             
-        //     input.addEventListener("keydown", (e) => {
-        //         if (e.key === "Backspace" && index > 0 && e.target.value === "") {
-        //             codes[index - 1].focus();
-        //         }
-        //     });
-        // });
-
-let inputElements = document.getElementsByClassName("code");
-console.log(inputElements.length)
-for(let i=0; i< inputElements.length; i++){
-	inputElements[i].addEventListener("keyup", (event)=>{
-		let currentElement = event.target ;
-		let code = event.key.charCodeAt(0);
-		console.log(event.key)
-		if(event.key === "Backspace"){
-			let prevElement = currentElement.previousElementSibling ;
-			if(prevElement){
-				prevElement.focus();
-			}
-			return ;
-		}
-		
-		
-		
-		if(code>=48 && code<=57){
-			let nextElement = currentElement.nextElementSibling ;
-			if(nextElement){
-				nextElement.focus() ;
-			}
-			
-		}else{
-			event.target.value="" ;
-		}
-	})
-	
-}
+            input.addEventListener("keydown", (e) => {
+                if (e.key === "Backspace" && index > 0 && e.target.value === "") {
+                    codes[index - 1].focus();
+                }
+            });
+        });
