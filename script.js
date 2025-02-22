df40eead12c28ad8695c6e428bbb359199ff471d
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2025-02-22 13:55:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
         const codes = document.querySelectorAll(".code");
 
+		// codes[0].focus()
+
         codes.forEach((input, index) => {
             input.addEventListener("input", (e) => {
                 if (e.target.value.length === 1 && index < codes.length - 1) {
